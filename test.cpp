a002d3241f19b8b2ea3f35b61010392ed3c15416
Prakash Jha <only2prakash@gmail.com>
2019-02-25 11:55:00 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 vector< int > pdiv{0,1,2,3,2,5,2,7,2,3,2,11,2,13,2,3,2,17,2,19,2,3,2,23,2,5,2,3};
-map<int,int> req[28];
+map<int, int> req[28];
+map<int, int> totreq[28];
 
 void pre()
 {
@@ -15,12 +16,28 @@ void pre()
 		}
 		req[i] = temp;
 	}
+
 	for(int i=2; i<=27; i++)
 	{
-		cout<<i<<" : ";
-		for(auto x: req[i]) cout<<x.first<<","<<x.second<<" "; cout<<"\n";
+		for(auto x: req[i])
+		{
+			totreq[i][x.first] += x.second;
+		}
+		for(auto x: totreq[i-1])
+		{
+			totreq[i][x.first] += x.second;
+		}
 	}
-cout<<"done\n";
+	// for(int i=2; i<=27; i++)
+	// {
+	// 	cout<<i<<" : ";
+	// 	for(auto x: totreq[i]) cout<<x.first<<","<<x.second<<" "; cout<<"\n";
+	// }
+}
+
+bool possible(int x)
+{
+	
 }
 
 int main()
@@ -39,17 +56,19 @@ int main()
 		{
 			int x = c-'a'+2;
 
-			while(x>1)
+			for(auto y:totreq[x])
 			{
-				avl[pdiv[x]]++;
-				x /= pdiv[x];
+				avl[y.first] += y.second;
 			}
-
-
 		}
 
+		for(int pt=2; pt<=27; pt++)
+		{
 
-
+		}
+		cout<<"Avl\n";
+		for(auto x: avl) cout<<x.first<<":"<<x.second<<"\n";
 	}
+cin>>t;
 }
 
